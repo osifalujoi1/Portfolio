c671b539dcb1d4f93ff66943d298b00467c2a07e
osifalujoi1 <osifalujoi1@mymail.nku.edu>
2024-05-13 20:30:21 -0400

    release

--- assets/js/main.js
+++ assets/js/main.js
@@ -130,11 +130,10 @@ themeButton.addEventListener('click', () => {
 const sr = ScrollReveal({ 
     origin: 'top',
     distance: '60px',
-    duration: 2500,
-    delay: 400,
+    duration: 2000,
+    delay: 300,
     // reset: true //animations repeat
 })
 
 sr.reveal('.home__perfil, .about__image, .contact__mail', {origin: 'right'})
-sr.reveal('.home__name, .home__info, .about__container, .section__title-1, .about__info, .contact__social, .contact__data', {origin: 'left'})
-sr.reveal('.resume__card, .projects__card', {interval: 100})
+sr.reveal('.home__name, .home__info, .about__container', {origin: 'left'})
