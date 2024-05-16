7135a6c47ceb267906d27005c17f36733c98fe07
osifalujoi1 <osifalujoi1@mymail.nku.edu>
2024-05-16 19:43:02 -0400

    palindrome project

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+const input = document.getElementById("text-input");
+const button = document.getElementById("check-btn");
+const result = document.getElementById("result");
+
+
+const check = () => {
+  const inputText = input.value.trim();
+  if(inputText === ""){
+    alert("Please input a value");
+    return;
+  }
+  const regex = /[^a-zA-Z0-9]/g;
+  const filteredInputText = (inputText.replace(regex, "")).toLowerCase();
+  
+  const isPalindrome = (filteredInputText.split("").reverse().join("") === filteredInputText) ? true : false;
+  
+  if(isPalindrome){
+    result.innerText = inputText + " is a palindrome";
+  }else{
+    result.innerText = inputText + " is not a palindrome";
+  }
+};
+
+button.addEventListener("click", check);
